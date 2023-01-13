3f7d04a864487509248b97ebd078d1128acd8944
faikgithub <faikalpbusiness@gmail.com>
2023-01-13 20:26:17 +0100

    game timer, winner

--- index.js
+++ index.js
@@ -149,6 +149,37 @@ function rectangularCollision({rectangle1, rectangle2})  {
   )
 }
 
+function determineWinner({player, enemy, timerId}){
+  clearTimeout(timerId)
+    document.querySelector('#displayText').style.display = 'flex'
+  if( player.health == enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie'
+  } else if(player.health>enemy.health){
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+    document.querySelector('#displayText').style.display = 'flex'
+  } else if(player.health<enemy.health){
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    document.querySelector('#displayText').style.display = 'flex'
+  }
+}
+
+let timer= 10
+let timerId
+function decreaseTimer() {
+timerId= setTimeout(decreaseTimer, 1000)
+if(timer>0) {timer--
+document.querySelector('#timer').innerHTML = timer
+}
+
+if(timer== 0){
+    determineWinner({player, enemy, timerId})
+
+}
+
+}
+
+decreaseTimer()
+
 function animate() {
   window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -197,6 +228,11 @@ enemy.velocity.x = 0
      enemy.isAttacking = false
      player.health -= 20
      document.querySelector('#playerHealth').style.width= player.health+'%'
+  }
+
+  //end game based on health
+  if(enemy.health<=0 || player.health<= 0) {
+    determineWinner({player, enemy, timerId})
 
   }
 
