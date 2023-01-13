c1c7e88e8cde48b254ee60a1315a65011243e46d
faikgithub <faikalpbusiness@gmail.com>
2023-01-13 15:59:35 +0100

    player, enemy controls and gravity

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const gravity = 0.2
+const gravity = 0.6
 
 class Sprite {
  constructor({position, velocity}){
    this.position = position
    this.velocity = velocity
    this.height= 150
+   this.lastkey
  }
 
 draw(){
@@ -60,34 +61,151 @@ velocity: {
 
 console.log(player)
 
+
+const keys = {
+  a:{
+    pressed: false
+  },
+
+  d:{
+    pressed: false
+  },
+
+  w:{
+    pressed: false
+  },
+
+  //ENEMY CONTROLS
+
+  ArrowLeft:{
+    pressed: false
+  },
+
+  ArrowRight:{
+    pressed: false
+  }
+
+
+
+}
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.clearRect(0,0, canvas.width, canvas.height)
   player.update()
   enemy.update()
+
+
+player.velocity.x = 0
+enemy.velocity.x = 0
+
+  if(keys.a.pressed && player.lastKey =='a'){
+    player.velocity.x = -5
+  } else if(keys.d.pressed && player.lastKey =='d'){
+    player.velocity.x = 5
+  }
+
+    //enemy movement
+  if(keys.ArrowLeft.pressed && enemy.lastKey =='ArrowLeft'){
+    enemy.velocity.x = -5
+  } else if(keys.ArrowRight.pressed && enemy.lastKey =='ArrowRight'){
+    enemy.velocity.x = 5
+  }
 }
 
 animate()
 
 window.addEventListener('keydown', (event) =>{
+
+  console.log(event.key);
+
+  switch(event.key){
+    //TO GO RIGHT
+    case 'd':
+
+      keys.d.pressed = true
+      player.lastKey = 'd'
+
+    break
+   //TO GO LEFT
+    case 'a':
+
+      keys.a.pressed = true
+      player.lastKey = 'a'
+    break
+
+   // JUMP
+    case 'w':
+
+      player.velocity.y= -10
+    break
+
+
+    //ENEMY KEYS
+
+    //TO GO RIGHT
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true
+      enemy.lastKey='ArrowRight'
+
+    break
+   //TO GO LEFT
+    case 'ArrowLeft':
+
+      keys.ArrowLeft.pressed = true
+      enemy.lastKey = 'ArrowLeft'
+    break
+
+   // JUMP
+    case 'ArrowUp':
+
+      enemy.velocity.y= -10
+    break
+  }
+
+
+console.log(event.key)
+})
+
+window.addEventListener('keyup', (event) =>{
   switch(event.key){
     case 'd':
 
-      player.velocity.x = 1
-
+      keys.d.pressed = false
     break
-  }
 
-  switch(event.key){
     case 'a':
 
-      player.velocity.x = -1
+      keys.a.pressed = false
+
+    break
+
+    case 'w':
+
+      keys.w.pressed = false
+
+    break
+
+    //ENEMY KEYS
+
+    case 'ArrowRight':
+
+      keys.ArrowRight.pressed = false
+    break
+
+    case 'ArrowLeft':
+
+      keys.ArrowLeft.pressed = false
+
+    break
+
+    case 'ArrowUp':
+
+      keys.ArrowUp.pressed = false
 
     break
   }
 
-
- console.log(event);
-
+console.log(event.key)
 })
