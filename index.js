8b3ee30d61bb24828a9b56c0990939f65677e899
faikgithub <faikalpbusiness@gmail.com>
2023-01-14 11:15:13 +0100

    background and categorizing

--- index.js
+++ index.js
@@ -8,71 +8,17 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
-class Sprite {
- constructor({position, velocity, color = 'red', offset}){
-   this.position = position
-   this.velocity = velocity
-   this. width= 50
-   this.height= 150
-   this.lastkey
-   this.attackBox = {
-     position: {
-       x: this.position.x,
-       y: this.position.y
-     },
-     offset,
-   width: 100,
-   height: 50
-   }
-   this.color = color
-   this.isAttacking
-   this.health = 100
- }
 
-draw(){
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  },
+    imageSrc: './img/background.png'
 
-  c.fillStyle = 'this.color'
-  c.fillRect(this.position.x, this.position.y, this.width, this.height)
+})
 
-
-
-  //    attack Box
-
-  if(this.isAttacking){
-
-   c.fillStyle ='green'
-   c.fillRect(
-     this.attackBox.position.x,
-     this.attackBox.position.y,
-     this.attackBox.width,
-     this.attackBox.height
-   )
-   }
-
-}
-
-update(){
-  this.draw()
-  this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-  this.attackBox.position.y = this.position.y
-
-  this.position.x += this.velocity.x
-  this.position.y += this.velocity.y
-
-  if(this.position.y + this.height + this.velocity.y >= canvas.height){
-    this.velocity.y = 0
-  } else this.velocity.y += gravity
- }
-
- attack(){
-   this.isAttacking = true
-   setTimeout(() => {
-     this.isAttacking=false
-   }, 100)
- }
-}
-
-const player = new Sprite({
+const player = new Fighter({
 position:  {
  x: 0,
  y: 0
@@ -88,7 +34,7 @@ velocity: {
 })
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
 position:  {
  x: 400,
  y: 100
@@ -131,52 +77,8 @@ const keys = {
   ArrowRight:{
     pressed: false
   }
-
-
-
 }
 
-function rectangularCollision({rectangle1, rectangle2})  {
-
-  return(
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-    rectangle2.position.x &&
-     rectangle1.attackBox.position.x <=
-     rectangle2.position.x + rectangle2.width &&
-     rectangle1.attackBox.position.y + rectangle1.attackBox.height>=
-     rectangle2.position.y &&
-     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  )
-}
-
-function determineWinner({player, enemy, timerId}){
-  clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-  if( player.health == enemy.health) {
-    document.querySelector('#displayText').innerHTML = 'Tie'
-  } else if(player.health>enemy.health){
-    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-    document.querySelector('#displayText').style.display = 'flex'
-  } else if(player.health<enemy.health){
-    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-    document.querySelector('#displayText').style.display = 'flex'
-  }
-}
-
-let timer= 10
-let timerId
-function decreaseTimer() {
-timerId= setTimeout(decreaseTimer, 1000)
-if(timer>0) {timer--
-document.querySelector('#timer').innerHTML = timer
-}
-
-if(timer== 0){
-    determineWinner({player, enemy, timerId})
-
-}
-
-}
 
 decreaseTimer()
 
@@ -184,6 +86,7 @@ function animate() {
   window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
   c.clearRect(0,0, canvas.width, canvas.height)
+  background.update()
   player.update()
   enemy.update()
 
@@ -288,8 +191,8 @@ window.addEventListener('keydown', (event) =>{
       enemy.velocity.y= -15
     break
 
-    case 'ArrowDown':
-    enemy.isAttacking = true
+    case'ArrowDown':
+    enemy.attack()
     break
   }
 
